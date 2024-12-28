ea22c2c0be68ada6ee5f532e599e4c09c1ce56ca
Cygnix <cygnix@noreply.invalid>
2024-12-29 00:05:40 +0100

    macOS: Attempt to fix RuneLite building
    This fixes patching when running the Mac .app through the inclusion of jdk.jartool into the bundled JRE

--- build-osx-x64.sh
+++ build-osx-x64.sh
@@ -30,8 +30,7 @@ build() {
     cp osx/runelite.icns $APPBASE/Contents/Resources/icons.icns
 
     tar zxf mac64_jre.tar.gz
-    mkdir $APPBASE/Contents/Resources/jre
-    mv jdk-$MAC_AMD64_VERSION-jre/Contents/Home/* $APPBASE/Contents/Resources/jre
+    jlink --module-path "jdk-$MAC_AMD64_VERSION/jmods" --add-modules java.base,java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.management.rmi,java.naming,java.net.http,java.prefs,java.rmi,java.scripting,java.se,java.security.jgss,java.security.sasl,java.smartcardio,java.sql,java.sql.rowset,java.transaction.xa,java.xml,java.xml.crypto,jdk.accessibility,jdk.charsets,jdk.crypto.cryptoki,jdk.crypto.ec,jdk.dynalink,jdk.httpserver,jdk.internal.vm.ci,jdk.internal.vm.compiler,jdk.internal.vm.compiler.management,jdk.jdwp.agent,jdk.jfr,jdk.jsobject,jdk.localedata,jdk.management,jdk.management.agent,jdk.management.jfr,jdk.naming.dns,jdk.naming.rmi,jdk.net,jdk.sctp,jdk.security.auth,jdk.security.jgss,jdk.unsupported,jdk.xml.dom,jdk.zipfs,jdk.jartool --no-man-pages --compress=2 --output $APPBASE/Contents/Resources/jre
 
     echo Setting world execute permissions on RuneLite
     pushd $APPBASE
